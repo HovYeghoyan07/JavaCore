351b21255c965d6ed7a09d5e8583c16548ca16e2
Hovhannes Yeghoyan <hovhannesyeghoyan491@gmail.com>
2024-07-08 14:11:15 +0400

    homework1

---
+++ src/homework1/ForIfExamples.java
@@ -0,0 +1,35 @@
+package homework1;
+
+public class ForIfExamples {
+    public static void main(String[] args) {
+
+        System.out.print("1.");
+        for (int x = 1; x < 10; x++) {
+            System.out.print(x);
+        }
+        System.out.println();
+        int b = 0;
+        for (int a = 1; a <= 100; a++) {
+            b += a;
+        }
+        System.out.println("2." + b);
+
+        System.out.print("3.");
+        for (int x = 50; x > 1; x--) {
+            System.out.print(x);
+        }
+        System.out.println();
+
+        System.out.print("4.");
+        for (int x = 1; x < 20; x = x + 2) {
+            System.out.print(x);
+
+        }
+        System.out.println();
+        int y = 0;
+        for (int x = 0; x < 100; x = x + 2) {
+            y = y + 1;
+        }
+        System.out.println("5." + y);
+    }
+}
